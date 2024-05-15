b4e01ee230ab6cb613a54a8acaa6860dfae7080d
ChirlaVishnuRupa <chirlavishnurupa@gmail.com>
2024-05-15 19:02:09 +0530

    Add Initial Files

---
+++ src/Components/Weather.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import "./css/style.css";
+
+const Weather = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("visakhapatnam");
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=24ba35f241e3bed99e2b41f03d65af0b`;
+      const response = await fetch(url);
+      const resJson = await response.json();
+      setCity(resJson.main);
+    };
+    fetchApi();
+  }, [search]);
+  return (
+    <>
+      <div className="box">
+        <div className="inputData">
+          <input
+            type="search"
+            className="inputFeild"
+            value={search}
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          ></input>
+        </div>
+
+        {!city ? (
+          <p className="nodata">No data found</p>
+        ) : (
+          <>
+            <div className="info">
+              <h2 className="location"><i className="fas fa-street-view"></i>        {search}</h2>
+              <h1 className="temp">{city.temp} °Cel</h1>
+              <h3 className="tempmin_max">
+                Min:{city.temp_min}°Cel | Max:{city.temp_max}°Cel
+              </h3>
+            </div>
+          </>
+        )}
+
+        <div className="wave -one"></div>
+        <div className="wave -two"></div>
+        <div className="wave -three"></div>
+      </div>
+    </>
+  );
+};
+
+export default Weather;
